2e64fac086e4950098dabdcfd5653977239c24de
Sudhanshu Yadav <sudhanshuyadav2@gmail.com>
2020-08-05 19:43:09 +0530

    - Remove children when its hidden. Improves React + DOM performance. - Add prop to keep the child when closed. - Use onTransitionEnd instead of setTimeout. - Forward remaining props on root element

--- src/index.js
+++ src/index.js
@@ -1,81 +1,99 @@
-import React, { useRef, useState, useEffect } from 'react'
-import PropTypes from 'prop-types'
+import React, { useRef, useState, useEffect } from 'react';
+import PropTypes from 'prop-types';
 
-const Collapsable = ({
+const Collapsible = ({
+  isOpen,
+  minAnimationDuration,
+  maxAnimationDuration,
+  speedDivider,
+  easing,
+  children,
+  keepChildrenOnClose,
+  ...restProps
+}) => {
+  const content = useRef();
+  const [state, setState] = useState({
+    height: isOpen ? 'auto' : 0,
+    speed: 0,
+  });
+
+  const [effectiveChildren, setChildren] = useState(children);
+
+  const handleClose = () => {
+    if (!isOpen) {
+      content.current.style.visibility = 'hidden';
+
+      if (!keepChildrenOnClose) {
+        // remove child
+        setChildren(null);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (isOpen) {
+      const contentHeight = content.current.scrollHeight;
+      const time = contentHeight / speedDivider;
+      const animation =
+        time < minAnimationDuration
+          ? minAnimationDuration
+          : time > maxAnimationDuration
+          ? maxAnimationDuration
+          : time;
+      content.current.style.visibility = 'visible';
+      setChildren(children);
+      setState({
+        ...state,
+        height: contentHeight,
+        speed: animation,
+      });
+    } else {
+      setState({
+        ...state,
+        height: 0,
+      });
+    }
+  }, [
     isOpen,
     minAnimationDuration,
     maxAnimationDuration,
     speedDivider,
-    easing,
-    children
-}) => {
-    const content = useRef()
-    const [state, setState] = useState({
-        height: isOpen ? 'auto' : 0,
-        speed: 0
-    })
+    children,
+    effectiveChildren,
+  ]);
 
-    useEffect(() => {
-        if (isOpen) {
-            const contentHeight = content.current.scrollHeight
-            const time = contentHeight / speedDivider
-            const animation =
-                time < minAnimationDuration
-                    ? minAnimationDuration
-                    : time > maxAnimationDuration
-                    ? maxAnimationDuration
-                    : time
-            content.current.style.visibility = 'visible'
-            setState({
-                ...state,
-                height: contentHeight,
-                speed: animation
-            })
-        } else {
-            setState({
-                ...state,
-                height: 0
-            })
-            setTimeout(() => {
-                content.current.style.visibility = 'hidden'
-            }, state.speed * speedDivider)
-        }
-    }, [
-        isOpen,
-        minAnimationDuration,
-        maxAnimationDuration,
-        speedDivider,
-        children
-    ])
+  return (
+    <div
+      {...restProps}
+      style={{
+        overflow: 'hidden',
+        height: state.height,
+        transition: `height ${state.speed}s ${easing}`,
+      }}
+      onTransitionEnd={handleClose}
+    >
+      <div ref={content} style={{ overflow: 'auto' }}>
+        {effectiveChildren}
+      </div>
+    </div>
+  );
+};
 
-    return (
-        <div
-            style={{
-                overflow: 'hidden',
-                height: state.height,
-                transition: `height ${state.speed}s ${easing}`
-            }}>
-            <div ref={content} style={{ overflow: 'auto' }}>
-                {children}
-            </div>
-        </div>
-    )
-}
+Collapsible.propTypes = {
+  isOpen: PropTypes.bool,
+  minAnimationDuration: PropTypes.number,
+  maxAnimationDuration: PropTypes.number,
+  speedDivider: PropTypes.number,
+  easing: PropTypes.string,
+  keepChildrenOnClose: PropTypes.bool,
+};
 
-Collapsable.propTypes = {
-    isOpen: PropTypes.bool,
-    minAnimationDuration: PropTypes.number,
-    maxAnimationDuration: PropTypes.number,
-    speedDivider: PropTypes.number,
-    easing: PropTypes.string
-}
+Collapsible.defaultProps = {
+  isOpen: false,
+  minAnimationDuration: 0.3,
+  maxAnimationDuration: 1,
+  speedDivider: 1000,
+  easing: 'ease-in-out',
+};
 
-Collapsable.defaultProps = {
-    isOpen: false,
-    minAnimationDuration: 0.3,
-    maxAnimationDuration: 1,
-    speedDivider: 1000,
-    easing: 'ease-in-out'
-}
-
-export default Collapsable
+export default Collapsible;
