abc4379de6e0394e8bdd7d56a89824cf9c64ce83
Sudhanshu Yadav <sudhanshuyadav2@gmail.com>
2020-08-12 15:36:58 +0530

    - Add react-dom as peer dependency and dev dependency - Use boolean flag instead of storing children in state. - Remove spread props and rename the name to react-collapsable

--- src/index.js
+++ src/index.js
@@ -1,99 +1,95 @@
-import React, { useRef, useState, useEffect } from 'react';
-import PropTypes from 'prop-types';
+import React, { useRef, useState, useEffect } from 'react'
+import PropTypes from 'prop-types'
 
-const Collapsible = ({
-  isOpen,
-  minAnimationDuration,
-  maxAnimationDuration,
-  speedDivider,
-  easing,
-  children,
-  keepChildrenOnClose,
-  ...restProps
-}) => {
-  const content = useRef();
-  const [state, setState] = useState({
-    height: isOpen ? 'auto' : 0,
-    speed: 0,
-  });
-
-  const [effectiveChildren, setChildren] = useState(children);
-
-  const handleClose = () => {
-    if (!isOpen) {
-      content.current.style.visibility = 'hidden';
-
-      if (!keepChildrenOnClose) {
-        // remove child
-        setChildren(null);
-      }
-    }
-  };
-
-  useEffect(() => {
-    if (isOpen) {
-      const contentHeight = content.current.scrollHeight;
-      const time = contentHeight / speedDivider;
-      const animation =
-        time < minAnimationDuration
-          ? minAnimationDuration
-          : time > maxAnimationDuration
-          ? maxAnimationDuration
-          : time;
-      content.current.style.visibility = 'visible';
-      setChildren(children);
-      setState({
-        ...state,
-        height: contentHeight,
-        speed: animation,
-      });
-    } else {
-      setState({
-        ...state,
-        height: 0,
-      });
-    }
-  }, [
+const Collapsable = ({
     isOpen,
     minAnimationDuration,
     maxAnimationDuration,
     speedDivider,
+    easing,
     children,
-    effectiveChildren,
-  ]);
+    keepChildrenOnClose
+}) => {
+    const content = useRef()
+    const [state, setState] = useState({
+        height: isOpen ? 'auto' : 0,
+        speed: 0
+    })
 
-  return (
-    <div
-      {...restProps}
-      style={{
-        overflow: 'hidden',
-        height: state.height,
-        transition: `height ${state.speed}s ${easing}`,
-      }}
-      onTransitionEnd={handleClose}
-    >
-      <div ref={content} style={{ overflow: 'auto' }}>
-        {effectiveChildren}
-      </div>
-    </div>
-  );
-};
+    const [keepChildren, toggleChildren] = useState(isOpen)
 
-Collapsible.propTypes = {
-  isOpen: PropTypes.bool,
-  minAnimationDuration: PropTypes.number,
-  maxAnimationDuration: PropTypes.number,
-  speedDivider: PropTypes.number,
-  easing: PropTypes.string,
-  keepChildrenOnClose: PropTypes.bool,
-};
+    const handleClose = () => {
+        if (!isOpen) {
+            content.current.style.visibility = 'hidden'
 
-Collapsible.defaultProps = {
-  isOpen: false,
-  minAnimationDuration: 0.3,
-  maxAnimationDuration: 1,
-  speedDivider: 1000,
-  easing: 'ease-in-out',
-};
+            if (!keepChildrenOnClose) {
+                // remove child
+                toggleChildren(false)
+            }
+        }
+    }
 
-export default Collapsible;
+    useEffect(() => {
+        if (isOpen) {
+            const contentHeight = content.current.scrollHeight
+            const time = contentHeight / speedDivider
+            const animation =
+                time < minAnimationDuration
+                    ? minAnimationDuration
+                    : time > maxAnimationDuration
+                    ? maxAnimationDuration
+                    : time
+            content.current.style.visibility = 'visible'
+            toggleChildren(true)
+            setState({
+                ...state,
+                height: contentHeight,
+                speed: animation
+            })
+        } else {
+            setState({
+                ...state,
+                height: 0
+            })
+        }
+    }, [
+        isOpen,
+        minAnimationDuration,
+        maxAnimationDuration,
+        speedDivider,
+        keepChildren
+    ])
+
+    return (
+        <div
+            style={{
+                overflow: 'hidden',
+                height: state.height,
+                transition: `height ${state.speed}s ${easing}`
+            }}
+            onTransitionEnd={handleClose}>
+            <div ref={content} style={{ overflow: 'auto' }}>
+                {keepChildren && children}
+            </div>
+        </div>
+    )
+}
+
+Collapsable.propTypes = {
+    isOpen: PropTypes.bool,
+    minAnimationDuration: PropTypes.number,
+    maxAnimationDuration: PropTypes.number,
+    speedDivider: PropTypes.number,
+    easing: PropTypes.string,
+    keepChildrenOnClose: PropTypes.bool
+}
+
+Collapsable.defaultProps = {
+    isOpen: false,
+    minAnimationDuration: 0.3,
+    maxAnimationDuration: 1,
+    speedDivider: 1000,
+    easing: 'ease-in-out'
+}
+
+export default Collapsable
